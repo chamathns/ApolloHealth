048ef34a43c014b8408dabb46b897cf925cc629b
Rakhitha <rakhitharr@live.com>
2019-12-08 21:55:21 +0530

    testing spinner functionality

--- app/src/main/java/com/example/apollohealth/MainActivity.java
+++ app/src/main/java/com/example/apollohealth/MainActivity.java
@@ -2,13 +2,55 @@ package com.example.apollohealth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner timeSpinner;
+//    private View mainView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addItemsSpinner();
     }
+
+    public void addItemsSpinner() {
+//        mainView = (View) findViewById(R.id.view);
+
+        timeSpinner = (Spinner) findViewById(R.id.timeSpinner);
+        timeSpinner.setOnItemSelectedListener(this);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.timeframe_report_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        timeSpinner.setAdapter(adapter);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        view.
+        Toast.makeText(parent.getContext(),
+                "Report Type : " + parent.getItemAtPosition(pos).toString(),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+
+
+
+
 }
