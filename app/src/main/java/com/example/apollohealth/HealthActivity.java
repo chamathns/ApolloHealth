35aa6f7f57cd018c42f3e18148ccb315fa6dea1c
Rakhitha <rakhitharr@live.com>
2019-12-14 20:12:08 +0530

    init pedometer test

--- app/src/main/java/com/example/apollohealth/HealthActivity.java
+++ app/src/main/java/com/example/apollohealth/HealthActivity.java
@@ -2,22 +2,74 @@ package com.example.apollohealth;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HealthActivity extends Activity {
+public class HealthActivity extends Activity implements SensorEventListener, StepListener {
+
+    private TextView tvSteps;
+    private Button startBtn;
+    private Button endBtn;
+    private StepDetector simpleStepDetector;
+    private SensorManager sensorManager;
+    private Sensor accel;
+    private static final String TEXT_NUM_STEPS = "Number of Steps: ";
+    private int numSteps;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_health);
         addBottomNavigation();
+
+        // Get an instance of the SensorManager
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        simpleStepDetector = new StepDetector();
+        simpleStepDetector.registerListener(this);
+
+        tvSteps = (TextView) findViewById(R.id.test_text);
+        startBtn = (Button) findViewById(R.id.startBtn);
+        endBtn = (Button) findViewById(R.id.endBtn);
+
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                numSteps = 0;
+                sensorManager.registerListener(HealthActivity.this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+
+            }
+        });
+
+
+        endBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                sensorManager.unregisterListener(HealthActivity.this);
+
+            }
+        });
+
+
+
+
 
     }
 
@@ -50,5 +102,24 @@ public class HealthActivity extends Activity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            simpleStepDetector.updateAccel(
+                    event.timestamp, event.values[0], event.values[1], event.values[2]);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void step(long timeNs) {
+        numSteps++;
+        tvSteps.setText(TEXT_NUM_STEPS + numSteps);
     }
 }
---
+++ app/src/main/java/com/example/apollohealth/StepListener.java
@@ -0,0 +1,6 @@
+package com.example.apollohealth;
+
+public interface StepListener {
+
+    public void step(long timeNs);
+}
