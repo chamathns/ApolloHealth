0fe6a07b409528432d5a2576726c36423a62ee3a
Yumantha <yumanthadesilva@gmail.com>
2019-12-18 10:00:44 +0530

    create user data table

--- app/src/main/java/com/example/apollohealth/DatabaseHandler.java
+++ app/src/main/java/com/example/apollohealth/DatabaseHandler.java
@@ -1,6 +1,50 @@
 package com.example.apollohealth;
 
-public class DatabaseHandler {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
     public static final String DB_NAME = "Apollo.db";
+
+    //    user data table
+    public static final String TABLE_NAME = "user_table";
+    public static final String USER_TABLE_ID = "ID";
+    public static final String USER_TABLE_NAME = "NAME";
+    public static final String USER_TABLE_AGE = "AGE";
+    public static final String USER_TABLE_GENDER = "GENDER";
+    public static final String USER_TABLE_WEIGHT = "WEIGHT";
+    public static final String USER_TABLE_HEIGHT = "HEIGHT";
+
+    public DatabaseHandler(@Nullable Context context) {
+        super(context, DB_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        USER_TABLE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        USER_TABLE_NAME + " TEXT," +
+                        USER_TABLE_AGE + " INTEGER," +
+                        USER_TABLE_GENDER + " TEXT," +
+                        USER_TABLE_WEIGHT + " FLOAT," +
+                        USER_TABLE_HEIGHT + " FLOAT)"
+        );
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(
+                "DROP TABLE IF EXISTS " + TABLE_NAME
+        );
+        this.onCreate(sqLiteDatabase);
+    }
+
+//    health table
+//    app data table
 
 }
