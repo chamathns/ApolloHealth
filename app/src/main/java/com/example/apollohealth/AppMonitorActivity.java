126e38b9c1f1fc51824a73e4b6a76da5451606a2
Yumantha <yumanthadesilva@gmail.com>
2019-12-27 16:39:26 +0530

    app monitor activity UI  - hard coded data and UI

--- app/src/main/java/com/example/apollohealth/AppMonitorActivity.java
+++ app/src/main/java/com/example/apollohealth/AppMonitorActivity.java
@@ -14,6 +14,7 @@ import android.provider.Settings;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -31,6 +32,39 @@ import java.util.Map;
 public class AppMonitorActivity extends AppCompatActivity {
     public static final int MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS = 0;
     public static final String LOG_TAG = "AM_ACTIVITY";
+
+    ImageView app1Icon;
+    ImageView app2Icon;
+    ImageView app3Icon;
+    ImageView app4Icon;
+    ImageView app5Icon;
+    ImageView app6Icon;
+    ImageView app7Icon;
+    ImageView app8Icon;
+    ImageView app9Icon;
+    ImageView app10Icon;
+
+    TextView app1Name;
+    TextView app2Name;
+    TextView app3Name;
+    TextView app4Name;
+    TextView app5Name;
+    TextView app6Name;
+    TextView app7Name;
+    TextView app8Name;
+    TextView app9Name;
+    TextView app10Name;
+
+    TextView app1Usage;
+    TextView app2Usage;
+    TextView app3Usage;
+    TextView app4Usage;
+    TextView app5Usage;
+    TextView app6Usage;
+    TextView app7Usage;
+    TextView app8Usage;
+    TextView app9Usage;
+    TextView app10Usage;
 
     private class UsageStat {
         private String packageName;
@@ -66,7 +100,19 @@ public class AppMonitorActivity extends AppCompatActivity {
             int minutes = (seconds % 3600) / 60;
             seconds = (seconds % 3600) % 60;
 
-            return hours + "hrs " + minutes + "mins " + seconds + "seconds";
+            if (hours > 0) {
+                return hours + "hrs " + minutes + "mins " + seconds + "seconds";
+            }
+
+            if (minutes > 0) {
+                return minutes + "mins " + seconds + "seconds";
+            }
+
+            if (seconds > 0) {
+                return seconds + "seconds";
+            }
+
+            return "No usage";
         }
 
         public String getAppName() {
@@ -90,12 +136,94 @@ public class AppMonitorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_monitor);
 
-        List<UsageStat> usageStats = getTopUsedApps(1, 10);
+        app1Icon = findViewById(R.id.app1Icon);
+        app1Name = findViewById(R.id.app1Name);
+        app1Usage = findViewById(R.id.app1Usage);
 
-        for (UsageStat stat : usageStats) {
-            Log.v(LOG_TAG, stat.toString());
-            Log.v(LOG_TAG, "\n");
-        }
+        app2Icon = findViewById(R.id.app2Icon);
+        app2Name = findViewById(R.id.app2Name);
+        app2Usage = findViewById(R.id.app2Usage);
+
+        app3Icon = findViewById(R.id.app3Icon);
+        app3Name = findViewById(R.id.app3Name);
+        app3Usage = findViewById(R.id.app3Usage);
+
+        app4Icon = findViewById(R.id.app4Icon);
+        app4Name = findViewById(R.id.app4Name);
+        app4Usage = findViewById(R.id.app4Usage);
+
+        app5Icon = findViewById(R.id.app5Icon);
+        app5Name = findViewById(R.id.app5Name);
+        app5Usage = findViewById(R.id.app5Usage);
+
+        app6Icon = findViewById(R.id.app6Icon);
+        app6Name = findViewById(R.id.app6Name);
+        app6Usage = findViewById(R.id.app6Usage);
+
+        app7Icon = findViewById(R.id.app7Icon);
+        app7Name = findViewById(R.id.app7Name);
+        app7Usage = findViewById(R.id.app7Usage);
+
+        app8Icon = findViewById(R.id.app8Icon);
+        app8Name = findViewById(R.id.app8Name);
+        app8Usage = findViewById(R.id.app8Usage);
+
+        app9Icon = findViewById(R.id.app9Icon);
+        app9Name = findViewById(R.id.app9Name);
+        app9Usage = findViewById(R.id.app9Usage);
+
+        app10Icon = findViewById(R.id.app10Icon);
+        app10Name = findViewById(R.id.app10Name);
+        app10Usage = findViewById(R.id.app10Usage);
+
+        List<UsageStat> usageStats = getTopUsedApps(3, 10);
+
+        app1Icon.setImageDrawable(usageStats.get(0).getIcon());
+        app1Name.setText(usageStats.get(0).getAppName());
+        app1Usage.setText(usageStats.get(0).getUsageTimeString());
+
+        app2Icon.setImageDrawable(usageStats.get(1).getIcon());
+        app2Name.setText(usageStats.get(1).getAppName());
+        app2Usage.setText(usageStats.get(1).getUsageTimeString());
+
+        app3Icon.setImageDrawable(usageStats.get(2).getIcon());
+        app3Name.setText(usageStats.get(2).getAppName());
+        app3Usage.setText(usageStats.get(2).getUsageTimeString());
+
+        app4Icon.setImageDrawable(usageStats.get(3).getIcon());
+        app4Name.setText(usageStats.get(3).getAppName());
+        app4Usage.setText(usageStats.get(3).getUsageTimeString());
+
+        app5Icon.setImageDrawable(usageStats.get(4).getIcon());
+        app5Name.setText(usageStats.get(4).getAppName());
+        app5Usage.setText(usageStats.get(4).getUsageTimeString());
+
+        app6Icon.setImageDrawable(usageStats.get(5).getIcon());
+        app6Name.setText(usageStats.get(5).getAppName());
+        app6Usage.setText(usageStats.get(5).getUsageTimeString());
+
+        app7Icon.setImageDrawable(usageStats.get(6).getIcon());
+        app7Name.setText(usageStats.get(6).getAppName());
+        app7Usage.setText(usageStats.get(6).getUsageTimeString());
+
+        app8Icon.setImageDrawable(usageStats.get(7).getIcon());
+        app8Name.setText(usageStats.get(7).getAppName());
+        app8Usage.setText(usageStats.get(7).getUsageTimeString());
+
+        app9Icon.setImageDrawable(usageStats.get(8).getIcon());
+        app9Name.setText(usageStats.get(8).getAppName());
+        app9Usage.setText(usageStats.get(8).getUsageTimeString());
+
+        app10Icon.setImageDrawable(usageStats.get(9).getIcon());
+        app10Name.setText(usageStats.get(9).getAppName());
+        app10Usage.setText(usageStats.get(9).getUsageTimeString());
+
+//        for (UsageStat stat : usageStats) {
+//            Log.v(LOG_TAG, stat.toString());
+//            Log.v(LOG_TAG, "\n");
+//
+//            app1Icon.setImageDrawable(stat.getIcon());
+//        }
 
         addBottomNavigation();
 
